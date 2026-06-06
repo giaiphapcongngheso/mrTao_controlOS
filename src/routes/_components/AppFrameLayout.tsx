edc2bef25f1c8a7839cc2b8620273060458dbc82
NguyenTD <dinhnguyen.306py@gmail.com>
2026-06-06 16:01:06 +0700

    feat something

--- src/routes/_components/AppFrameLayout.tsx
+++ src/routes/_components/AppFrameLayout.tsx
@@ -197,7 +197,7 @@ function AppFrameLayout({
             <div className="flex items-center gap-5">{headerRight}</div>
           </header>
 
-          <div className="p-5 md:p-5 pb-20 md:pb-4 w-full space-y-3.5 font-sans text-sm text-slate-650 min-w-0 overflow-hidden">
+          <div className="p-5 md:p-5 pb-20 md:pb-4 w-full space-y-3.5 font-sans text-sm text-slate-650 min-w-0 flex-1 min-h-0 overflow-y-auto">
             {children}
           </div>
         </main>
